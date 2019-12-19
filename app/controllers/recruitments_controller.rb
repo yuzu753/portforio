3be7223f22848cad0df726d800123692af367b33
MAC User <macuser@MAC-no-MacBook-Air.local>
2019-12-19 20:43:48 +0900

    ransack add

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,6 +1,16 @@
 class RecruitmentsController < ApplicationController
   def index
-    @rucruitments = Recruitment.all
+    # 全件表示
+    @recruitments = Recruitment.all
+    @recrruitment = Recruitment.new
+    # 検索
+    @q = Recruitment.ransack(params[:q])
+    rectuitments = @q.result(distinct: true)
+  end
+
+  def search
+    @q = Recruitment.search(search_params)
+    @recruitments = @q.result(distinct: true)
   end
 
   def show
@@ -22,27 +32,30 @@ class RecruitmentsController < ApplicationController
     else
       redirect_to root_path
     end
-    #    redirect_to comlete_path
-    #    tag_list = params[:tags_name].split(",")
-    # if
-    #    @recruitment.save
-    #    @recruitment.save_recruitments(tag_list)
-    #  else
-    #    redirect_to new_recruitment_path
-    # end
   end
 
   def edit
+    @recruitment = Recruitment.find(params[:id])
   end
 
   def destroy
+    @recruitment = Recruitment.find(params[:id])
+    @recruitment.destroy
+    redirect_to recruitments_path
   end
 
   def update
+    @recruitment = Recruitment.find(params[:id])
+    if @recruitment.update
+      redirect_to recruitment_path(@recruitment.id)
+      flash[:update] = "投稿を更新しました"
+    else
+      render :edit
+    end
   end
-
   def confirmation
   end
+
 
   def complete
   end
@@ -51,4 +64,9 @@ class RecruitmentsController < ApplicationController
   def recruitment_params
       params.require(:recruitment).permit(:user_id, :category, :title, :body, :event_date, :area, :address, :recruitments_number)
   end
+
+  def search_params
+    params.require(:q).permit!
+  end
+
 end
