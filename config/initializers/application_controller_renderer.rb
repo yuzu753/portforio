9d39d65a49eca8d4a2a5ab2c18b83bb1d1c8f912
MAC User <macuser@MAC-no-MacBook-Air.local>
2019-12-23 14:01:43 +0900

    secret_key add

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'efdad7ad80df66d4923477a6859cb608191b34277952603445d74e0ca60548903f60622aa67e7fef4d0da91e0dd584eeef08da0c5ff5b0acce2beeabbd96130a'
